e17910b8a371ae7240ab4dce46c34b25d37f7999
李勃旺 <libw@csdn.net>
2020-07-16 23:21:30 +0800

    20200716, learn vue.js

--- Code/Lang/Go/hello/interface.go
+++ Code/Lang/Go/hello/interface.go
@@ -8,6 +8,29 @@ func main() {
 
 	testInterface(mouse)
 	testInterface(disk)
+
+	var a1 EmptyInterface = Cat{"red"}
+	var a2 EmptyInterface = Person2{"li", 30}
+	var a3 = "haha"
+	var a4 = 123
+	fmt.Println(a1, a2, a3, a4)
+
+	emptyInterface(a1)
+	emptyInterface(a2)
+
+	emptyInterface2(a3)
+	emptyInterface2(a4)
+
+	// value 任意类型的 map
+	map1 := make(map[string]interface{})
+	// value 任意类型的 slice
+	slice1 := make([]interface{}, 0, 10)
+	fmt.Println(map1, slice1)
+
+	mao := Mao{}
+	mao.testa()
+	mao.testb()
+	mao.testc()
 }
 
 type USB interface {
@@ -39,7 +62,59 @@ func (disk FlashDisk) end() {
 	fmt.Println("end:", disk.name)
 }
 
-func testInterface(usb USB)  {
+func testInterface(usb USB) {
 	usb.start()
 	usb.end()
+}
+
+// empty interface
+type EmptyInterface interface {
+}
+
+type Cat struct {
+	color string
+}
+
+type Person2 struct {
+	name string
+	age  int
+}
+
+// 传入任何类型的参数
+func emptyInterface(ei EmptyInterface)  {
+	fmt.Println(ei)
+}
+
+func emptyInterface2(ei interface{})  {
+	fmt.Println(ei)
+}
+
+type A interface {
+	testa()
+}
+
+type B interface {
+	testb()
+}
+
+type C interface {
+	A
+	B
+	testc()
+}
+
+type Mao struct {
+
+}
+
+func (m Mao) testa() {
+	fmt.Println("testa")
+}
+
+func (m Mao) testb() {
+	fmt.Println("testb")
+}
+
+func (m Mao) testc() {
+	fmt.Println("testc")
 }
--- Code/Lang/Python/appium.md
+++ Code/Lang/Python/appium.md
@@ -8,6 +8,9 @@
   - brew cask install android-sdk
 > link adb to sdk
   - ln -s /usr/local/bin/adb /usr/local/share/android-sdk/tools/adb
+> install appium-doctor
+  - sudo npm install appium-doctor -g
+  - brew install carthage
 
 ```shell
 export JAVA_HOME=$(/usr/libexec/java_home)/bin/java
